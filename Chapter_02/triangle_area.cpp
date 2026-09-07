bcc5c59c40eceaa28542be2f0d30d1b6f10f2eb3
ssempijja ibrahim <ibrahimssempijjassemuhire@gmail.com>
2026-09-07 18:32:28 +0300

    Add triangle area calculation program
    Implement triangle area calculation based on user input points.

--- Chapter_02/triangle_area.cpp
+++ Chapter_02/triangle_area.cpp
@@ -4,3 +4,25 @@
 // Area = sqrt(s(s-side1)(s-side2)(s-side3))
 // Example: Enter three points for a triangle: 1.5, -3.4, 4.6, 5, 9.5, -3.4 
 // The area of the triangle is 33.6
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double x1, y1, x2, y2, x3, y3; //my variables for the three points
+
+    cout << "Enter three points for a triangle: ";//my input prompt
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;//entering them in order
+      //evaluation of points side lengths mathematically
+    double side1 = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    double side2 = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2));
+    double side3 = sqrt(pow(x1 - x3, 2) + pow(y1 - y3, 2));
+
+    double s = (side1 + side2 + side3) / 2;//getting the essential s variable value for area calculation
+    
+    double area = sqrt(s * (s - side1) * (s - side2) * (s - side3));//standard area formula for s variable
+
+    cout << "The area of the triangle is " << area << endl;
+
+    return 0;
+}
